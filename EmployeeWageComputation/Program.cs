e6a30dcc5edaf3cc0aad0c4d08d40c67029e3c48
SaiVenkat12 <gsaiv1001@gmail.com>
2023-01-20 21:57:39 +0530

    [Sai Venkat] Added Employee  Montly Wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -12,32 +12,42 @@ namespace EmployeeWageComputation
         {
             const int isFullTime = 1;
             const int isPartTime = 2;
-            int empWagePerHr = 20;
+            const int empWagePerHr = 20;
             int empHrs = 0;
+            int totalEmpHrs = 0;
             int empDailyWage = 0;
+            int empMontlyWage = 0;
+            int empWorkinDaysPerMonth = 20;
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
             //UC1- Employee Attendance
             Random random = new Random();
-            int empAttendance = random.Next(0, 3);
-            //UC3- Add Part Time Employee
-            //UC4- Switch Case
-            switch(empAttendance)
+            
+            for (int i=0; i<empWorkinDaysPerMonth; i++)
             {
-                case isFullTime:
-                    Console.WriteLine("Employee is FullTime");
-                    empHrs = 8;
-                    break;
-                case isPartTime:
-                    Console.WriteLine("Employee is PartTime");
-                    empHrs = 4;
-                    break;
+                int empAttendance = random.Next(0, 3);
+                //UC3- Add Part Time Employee
+                //UC4- Switch Case
+                switch (empAttendance)
+                {
+                    case isFullTime:
+                        Console.WriteLine("Employee is FullTime");
+                        empHrs = 8;
+                        break;
+                    case isPartTime:
+                        Console.WriteLine("Employee is PartTime");
+                        empHrs = 4;
+                        break;
                     default:
-                    Console.WriteLine("Employee is Absent");
-                    break;
+                        Console.WriteLine("Employee is Absent");
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
             }
             //UC2- Employee Daily Wage
-            empDailyWage = empWagePerHr * empHrs;
-            Console.WriteLine("Daily Wage of Emplyoee is " + empDailyWage);
+            //UC5- Employee Monthly Wage
+            empMontlyWage = empWagePerHr * totalEmpHrs;
+            Console.WriteLine("Montly Wage of Emplyoee is " + empMontlyWage);
             Console.ReadLine();
         }
     }
