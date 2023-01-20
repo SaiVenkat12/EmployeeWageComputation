91619574f92f88b6420efba26fd6be6d05698372
SaiVenkat12 <gsaiv1001@gmail.com>
2023-01-20 21:22:47 +0530

    [Sai Venkat] Added Employee Daily Wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -10,20 +10,28 @@ namespace EmployeeWageComputation
     {
         public static void Main(string[] args)
         {
-            int isPresent = 1;
+            const int isFullTime = 1;
+            int empWagePerHr = 20;
+            int empHrs = 0;
+            int empDailyWage = 0;
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
             //UC1- Employee Attendance
             Random random = new Random();
             int empAttendance = random.Next(0, 2);
 
-            if (empAttendance == isPresent)
+            if (empAttendance == isFullTime)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
+
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
             }
+            //UC2- Employee Daily Wage
+            empDailyWage = empWagePerHr * empHrs;
+            Console.WriteLine("Daily Wage of Emplyoee is " + empDailyWage);
             Console.ReadLine();
         }
     }
