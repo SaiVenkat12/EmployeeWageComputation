4edd1be1889bd0fac9bee9e95a696cc5257aa261
SaiVenkat12 <gsaiv1001@gmail.com>
2023-01-20 21:29:57 +0530

    [Sai Venkat] Added Part Time Employee

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -11,20 +11,25 @@ namespace EmployeeWageComputation
         public static void Main(string[] args)
         {
             const int isFullTime = 1;
+            const int isPartTime = 2;
             int empWagePerHr = 20;
             int empHrs = 0;
             int empDailyWage = 0;
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
             //UC1- Employee Attendance
             Random random = new Random();
-            int empAttendance = random.Next(0, 2);
-
+            int empAttendance = random.Next(0, 3);
+            //UC3-Add Part Time Employee
             if (empAttendance == isFullTime)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is FullTime");
                 empHrs = 8;
-
             }
+            else if(empAttendance== isPartTime)
+            {
+                Console.WriteLine("Employee is PartTime");
+                empHrs = 4;
+            }           
             else
             {
                 Console.WriteLine("Employee is Absent");
