5234e564a20eafc9f90f0c5df78843d8ce257a35
SaiVenkat12 <gsaiv1001@gmail.com>
2023-01-20 21:11:49 +0530

    [Sai Venkat] Modified Employee Wage Computation

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -10,7 +10,20 @@ namespace EmployeeWageComputation
     {
         public static void Main(string[] args)
         {
+            int isPresent = 1;
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            //UC1- Employee Attendance
+            Random random = new Random();
+            int empAttendance = random.Next(0, 2);
+
+            if (empAttendance == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
             Console.ReadLine();
         }
     }
