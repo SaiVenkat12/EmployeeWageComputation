1c45dcd64a3b573560166619aef4934a3971b5c1
SaiVenkat12 <gsaiv1001@gmail.com>
2023-02-08 21:53:17 +0530

    [Sai Venkat] Added -ManageWageForMultipleCompanies

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -11,19 +11,19 @@ namespace EmployeeWageComputation
         
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            Console.WriteLine("  Welcome to Employee Wage Computation Problem");
             //UC7-Use Class Method and Variables
             //UC8-Multiple companies
             EmpWageBuilder Tata =new EmpWageBuilder("Tata", 30, 16, 60);
-            EmpWageBuilder Google = new EmpWageBuilder("Google", 20, 15, 80);
+            EmpWageBuilder google = new EmpWageBuilder("Google", 20, 15, 80);
             EmpWageBuilder Apple = new EmpWageBuilder("Apple", 15, 18, 100);
 
             Tata.EmpWage();
             Console.WriteLine(Tata.ToString());
-            Google.EmpWage();
+            google.EmpWage();
             Console.WriteLine(Apple.ToString());
             Apple.EmpWage();
-            Console.WriteLine(Google.ToString());
+            Console.WriteLine(google.ToString());
 
             Console.ReadLine();
         }             
