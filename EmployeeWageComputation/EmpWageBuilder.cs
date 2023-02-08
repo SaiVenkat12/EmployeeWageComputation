14c2a046fefc54a21d43447b5d0bcc0766eb43f0
SaiVenkat12 <gsaiv1001@gmail.com>
2023-02-08 21:27:05 +0530

    [Sai Venkat] Added Employee Wage for each company

---
+++ EmployeeWageComputation/EmpWageBuilder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class EmpWageBuilder
+    {
+        const int isFullTime = 1;
+        const int isPartTime = 2;
+
+        private string company;
+        private int empWagePerHr;
+        private int empWorkinDaysPerMonth;
+        private int empTotalWorkingHrs;
+        private int empMontlyWage;
+
+        public EmpWageBuilder(string company, int empWagePerHr, int empWorkinDaysPerMonth, int empTotalWorkingHrs)
+        {
+            this.company = company;
+            this.empWagePerHr = empWagePerHr;
+            this.empWorkinDaysPerMonth = empWorkinDaysPerMonth;
+            this.empTotalWorkingHrs = empTotalWorkingHrs;
+        }
+        public void EmpWage()
+        {
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+
+            //UC1- Employee Attendance
+            Random random = new Random();
+            //UC6-MaxHrs
+            while (totalWorkingDays < empWorkinDaysPerMonth && totalEmpHrs < empTotalWorkingHrs)
+            {
+                int empAttendance = random.Next(0, 3);
+                //UC3- Add Part Time Employee
+                //UC4- Switch Case
+                switch (empAttendance)
+                {
+                    case isFullTime:
+                        empHrs = 8;
+                        break;
+                    case isPartTime:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalWorkingDays++;
+                totalEmpHrs += empHrs;
+            }
+            //UC2- Employee Daily Wage
+            //UC5- Employee Monthly Wage
+            empMontlyWage = empWagePerHr * totalEmpHrs;
+            Console.WriteLine("Total Number of Hours is  " + totalEmpHrs + ", Total Number of days is " + totalWorkingDays + ", Montly Wage of Emplyoee is " + empMontlyWage);
+        }
+        //UC9-Total Wage for each Company
+        public string ToString()
+        {
+            return "Company :" + this.company + " TotalWage =" + this.empMontlyWage;
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -8,54 +8,24 @@ namespace EmployeeWageComputation
 {
     public class Program
     {
-        const int isFullTime = 1;
-        const int isPartTime = 2;
+        
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
             //UC7-Use Class Method and Variables
-            Program obj=new Program();
-            obj.EmpWage("Tata", 30, 16, 60);
-            obj.EmpWage("Google", 20, 15, 80);
-            obj.EmpWage("Apple", 15, 18, 100);
+            EmpWageBuilder Tata=new EmpWageBuilder("Tata", 30, 16, 60);
+            EmpWageBuilder Google = new EmpWageBuilder("Google", 20, 15, 80);
+            EmpWageBuilder Apple = new EmpWageBuilder("Apple", 15, 18, 100);
+
+            Tata.EmpWage();
+            Console.WriteLine(Tata.ToString());
+            Google.EmpWage();
+            Console.WriteLine(Google.ToString());
+            Apple.EmpWage();
+            Console.WriteLine(Apple.ToString());
             Console.ReadLine();
         }
         //UC8-Multiple companies
-        public void EmpWage(string company, int empWagePerHr, int empWorkinDaysPerMonth, int empTotalWorkingHrs)
-        {
-            int empHrs = 0;
-            int totalEmpHrs = 0;
-            int empMontlyWage = 0;
-            int totalWorkingDays = 0;
-
-            //UC1- Employee Attendance
-            Random random = new Random();
-            //UC6-MaxHrs
-            while (totalWorkingDays < empWorkinDaysPerMonth && totalEmpHrs < empTotalWorkingHrs)
-            {
-                int empAttendance = random.Next(0, 3);
-                //UC3- Add Part Time Employee
-                //UC4- Switch Case
-                switch (empAttendance)
-                {
-                    case isFullTime:
-                        empHrs = 8;
-                        break;
-                    case isPartTime:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalWorkingDays++;
-                totalEmpHrs += empHrs;
-            }
-            //UC2- Employee Daily Wage
-            //UC5- Employee Monthly Wage
-            empMontlyWage = empWagePerHr * totalEmpHrs;
-            Console.WriteLine("Company : " + company);
-            Console.WriteLine("Total Number of Hours is  " + totalEmpHrs + ", Total Number of days is " + totalWorkingDays+", Montly Wage of Emplyoee is " + empMontlyWage);
-        }
+        
     }
 }
